ea6b2073d939eb09aa0ed511150e3c66a01d2e3a
Michael Pan <micpan0312@gmail.com>
2023-08-23 16:23:03 -0700

    carousel img bkg updated

--- src/home/components/carousel/index.jsx
+++ src/home/components/carousel/index.jsx
@@ -9,19 +9,22 @@ const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 const CarouselBox = () => {
   const defualtData = [
     {
-      img: "car1.png",
-      title: "ZooEx is a decentralized",
-      info: "ZooEx is a decentralized margin trading protocol that enables leverage trading for any tokens",
+      img: "car1_updated.png",
+      bkg: "car1-bkg.png",
+      title: "Deep Liquidity for Any Pairs",
+      info: "Provide traders with the deepest liquidity possible across all trading pairs by leveraging the liquidity pools on DEXs. Unlike centralized exchanges, ZooEx never trades against its users, ensuring that the platform operates with transparency and fairness.",
     },
     {
-      img: "car2.png",
-      title: "ZooEx is a decentralized",
-      info: "ZooEx is a decentralized margin trading protocol that enables leverage trading for any tokens",
+      img: "car2_updated.png",
+      bkg: "car2-bkg.png",
+      title: "High Leverage at Low Cost",
+      info: "Offer competitive interest rates by utilizing lending protocols from top providers like Aave. This allows us to offer high leverage up to 5x on major trading pairs, providing traders with the ability to amplify their gains while keeping costs low.",
     },
     {
-      img: "car3.png",
-      title: "ZooEx is a decentralized",
-      info: "ZooEx is a decentralized margin trading protocol that enables leverage trading for any tokens",
+      img: "car3_updated.png",
+      bkg: "car3-bkg.png",
+      title: "Unique \"JackPot\" Trading Fees Distribution",
+      info: "ZooEx is revolutionizing the way trading fees are distributed by offering a unique \"JackPot\" program. We distribute 100% of trading fees, providing traders with the opportunity to earn more while adding an exciting element of chance to the trading experience.",
     },
   ];
 
@@ -36,6 +39,7 @@ const CarouselBox = () => {
           <div className={styles.box_left_img_box}>
             <div className={styles.box_left_img_value}>
               <img src={defualtData[activeStep].img} alt="" className={styles.img} />
+              <img src={defualtData[activeStep].bkg} alt="" className={styles.bkg} />
             </div>
           </div>
         </div>
