03989a82ea025b7e129f9847afdbd28ed24c4013
liaogt <liaogt123@qq.com>
2023-08-24 17:22:46 +0800

    update

--- src/home/components/joinZoo/index.jsx
+++ src/home/components/joinZoo/index.jsx
@@ -1,64 +1,79 @@
 import React from "react";
 import styles from "./index.module.css";
 import { Button, Menu, MenuItem } from "@mui/material";
+import { Height } from "@mui/icons-material";
 
 const JoinZoo = () => {
-    return (
-        <div className={styles.join_box}>
-            <div className={styles.join_title}>
-                Join the Zoo
-            </div>
-            <div className={styles.join_info_box}>
-                <Button className={styles.join_info} onClick={
-                    () => {
+  return (
+    <div className={styles.join_box}>
+      <div className={styles.join_title}>Join the Zoo</div>
+      <div className={styles.join_info_box}>
+        <Button className={styles.join_info} onClick={() => {}}>
+          <img
+            src="/twitter.svg"
+            style={{
+              paddingLeft: "20px",
+              paddingTop: "16px",
+              paddingBottom: "16px",
+            }}
+          ></img>
+          <div className={styles.join_info_text}>Twitter</div> 
+        </Button>
 
-                    }
-                }>
-                    <img src="/twitter.svg" style={{
-                        paddingLeft: "20px",
-                        paddingTop: "16px",
-                        paddingBottom: "16px"
-                    }}></img>
-                    <div className={styles.join_info_text}>Twitter</div>
-                </Button>
+        <Button className={styles.join_info}>
+          <img
+            src="/discord.svg"
+            style={{
+              paddingLeft: "20px",
+              paddingTop: "16px",
+              paddingBottom: "16px",
+            }}
+          ></img>
+          <div className={styles.join_info_text}>Discord</div>
+        </Button>
 
-                <Button className={styles.join_info}>
-                <img src="/discord.svg" style={{
-                        paddingLeft: "20px",
-                        paddingTop: "16px",
-                        paddingBottom: "16px"
-                    }}></img>
-                    <div className={styles.join_info_text}>Discord</div>
-                </Button>
+        <Button className={styles.join_info}>
+          <img
+            src="/telegram.svg"
+            style={{
+              paddingLeft: "20px",
+              paddingTop: "16px",
+              paddingBottom: "16px",
+            }}
+          ></img>
+          <div className={styles.join_info_text}>Telegram</div>
+        </Button>
 
-                <Button className={styles.join_info}>
-                <img src="/telegram.svg" style={{
-                        paddingLeft: "20px",
-                        paddingTop: "16px",
-                        paddingBottom: "16px"
-                    }}></img>
-                    <div className={styles.join_info_text}>Telegram</div>
-                </Button>
-
-                <Button className={styles.join_info}>
-                <img src="/reddit 1.png" style={{
-                        paddingLeft: "20px",
-                        paddingTop: "16px",
-                        paddingBottom: "16px"
-                    }}></img>
-                    <div className={styles.join_info_text}>Reddit</div>
-                </Button>
-
-            </div>
-            <img src="/Rectangle.png" style={{
-                width: "15%",
-                marginTop: "200px",
-                justtifyContent: "center",
-                marginLeft: "35%",
-
-            }}></img>
-        </div>
-    );
+        <Button className={styles.join_info}>
+          <img
+            src="/reddit 1.png"
+            style={{
+              paddingLeft: "20px",
+              paddingTop: "16px",
+              paddingBottom: "16px",
+            }}
+          ></img>
+          <div className={styles.join_info_text}>Reddit</div>
+        </Button>
+      </div>
+      <div style={{
+        width:"100%",
+        height:"auto",
+        justifyContent:"center"
+      }}>
+      <img
+        src="/Rectangle.jpg"
+        style={{
+          width: "15%",
+          marginTop: "200px",
+          justtifyContent: "center",
+        //   transform:"rotateY(160deg)"
+        //   marginLeft: "35%",
+        }}
+      ></img>
+      </div>
+    </div>
+  );
 };
 
 export default JoinZoo;
