d9a2a76dd8b82c859fceacf374e4b690ef0958aa
Michael P <micpan0312@gmail.com>
2023-09-26 09:29:49 -0700

    pointer & meta tag testing

--- pages/_document.js
+++ pages/_document.js
@@ -15,6 +15,7 @@ export default function MyDocument(props) {
         <meta name="theme-color" content={theme.palette.primary.main} />
         <link rel="shortcut icon" href="/favicon.ico" />
         <meta name="emotion-insertion-point" content="" />
+        <meta property="og:image" href="/thumbnail.jpg" />
         {emotionStyleTags}
       </Head>
       <body>
