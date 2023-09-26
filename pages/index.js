bafdea268381c746a6b975dccb5168a1883a794c
Michael P <micpan0312@gmail.com>
2023-09-26 10:36:43 -0700

    meta tag testing

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,32 @@ import * as React from "react";
 import { ThemeProvider } from "@mui/material";
 import theme from "../src/theme";
 import Home from "../src/home";
+import Head from "next/head";
 
 export default function Index() {
+  const imageUrl = '/thumbnail.jpg';
+
   return (
     <ThemeProvider theme={theme}>
+      <Head>
+        <title>Zoo</title>
+        <meta name="title" content="Zoo" />
+        <meta name="description" content="A decentralized margin trading protocol that enables leverage trading for any tokens" />
+
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://metatags.io/" />
+        <meta property="og:title" content="Zoo" />
+        <meta property="og:description" content="A decentralized margin trading protocol that enables leverage trading for any tokens" />
+        <meta property="og:image" content={imageUrl} />
+
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://metatags.io/" />
+        <meta property="twitter:title" content="Zoo" />
+        <meta property="twitter:description" content="A decentralized margin trading protocol that enables leverage trading for any tokens" />
+        <meta property="twitter:image" content={imageUrl} />
+      </Head>
       <Home></Home>
     </ThemeProvider>
   );
